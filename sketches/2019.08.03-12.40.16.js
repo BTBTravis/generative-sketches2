f38f7976fe17021d545b5a81cef7fd117b1fff3a
Travis Shears <btbtravis@gmail.com>
2019-08-11 12:36:52 +0200

    Render keys with print letters sketch_012

--- sketches/2019.08.03-12.40.16.js
+++ sketches/2019.08.03-12.40.16.js
@@ -6,64 +6,11 @@ const settings = {
     dimensions: [ 512, 512 ]
 };
 
-const keysJson = `["~","!","@","#","$","%","^","&","*","(",")","_","+","1","2","3","4","5","6","7","8","9","0","-","=","q","w","e","r","t","y","u","i","o","p","[","]","a","s","d","f","g","h","j","k","l",";","z","x","c","v","b","n","m",",",".","?","Q","W","E","R","T","Y","U","I","O","P","{","}",":","L","K","J","H","G","F","D","S","A","Z","X","C","V","B","N","M","<",">"]`;
-let keys = JSON.parse(keysJson);
-for (let i = 0; i < 16; i++) {
-    keys.push((i + 10).toString());
-}
-//keys = utils.shuffle(keys);
-
-//console.log(keys.reduce((i, carry) => {
-    //return i + '  ' + carry;
-//}));
-
-
-
-//document.addEventListener('keypress', (event) => {
-//keys.add(event.key);
-//});
 
 const colCount = 11;
 
-function handleSVGLoad(i) {
-    //console.log('i: ', i);
-}
-
-function tilt(l) {
-    const og = l.bounds.bottomCenter;
-    const segs = l.segments.map(s => {
-        const mv = (x, y) => {
-            const scale = Math.abs(y - og.y) / 20;
-            const diff = Math.abs(x - og.x);
-            if (x != og.x && scale > 1) {
-                if (x > og.x) {
-                    return [og.x + (diff * scale), y];
-                } else {
-                    return [og.x - (diff * scale), y];
-                }
-            }
-            return [x, y];
-        };
-        return new Segment({
-            point: mv(s.point.x, s.point.y),
-            handleOut: mv(s.handleOut.x, s.handleOut.y),
-            handleIn: [s.handleIn.x, s.handleIn.y],
-        });
-    });
-    let p = new Path({
-        fillColor: 'black',
-        //fullySelected: true
-    });
-    p.addSegments(segs);
-    //l.remove();
-    l.fillColor = 'pink';
-    console.log('segs: ', segs[0]);
-    console.log('l.segments', l.segments[0]);
-    console.log('p: ', p);
-    return p;
-}
-
-function drawGrid(i) {
+function drawGrid(i, letters) {
+    console.log({i, letters});
     //const keyWidth = i.bounds.width;
     //const maxWidth = view.bounds.width / colCount;
     //const scale = 1 / (keyWidth / maxWidth);
@@ -81,31 +28,33 @@ function drawGrid(i) {
         }
     }
 
-    //console.log(grid);
+    console.log({grid});
 
     // add letters
+    const gg = new Group();
     grid.children.forEach((g, i) => {
+        const l = letters[i];
+        //l.bringToFront();
+        //console.log({g, l, i});
+        l.translate(new Point(l.bounds.center.x * -1, l.bounds.center.y * -1));
+        l.translate(new Point(g.bounds.center.x, g.bounds.center.y - 35));
+        l.scale(0.70, l.bounds.bottomCenter);
+        //l.translate(g.bounds.center);
+        //g.addChild(l);
+        //console.log({g});
+        //l.bringToFront();
+        //l.fullySelected = true;
+        //l.bringToFront()
+        //l.addTo(g);
+
         //console.log('g: ', g);
-        var txt = new PointText({
-            point: g.bounds.center.add(new Point(0, -20)),
-            content: keys[i],
-            fillColor: 'black',
-            justification: 'center',
-            fontWeight: 'bold',
-            fontSize: 30
-        });
-        tilt(txt);
-        //console.log('txt: ', txt);
-        //txt.shear(1, 0, txt.bounds.bottomCenter);
-        //txt.skew(40, 0, txt.bounds.center);
-        //txt.skew(40, 0, txt.bounds.bottomCenter);
-        //txt.fullySelected = true;
-        g.addChild(txt);
+        //const l = letters[i];
+        //console.log({l});
+        //g.addChild(l);
+        gg.addChild(l);
     });
 
     i.remove();
-    console.log('grid: ', grid);
-
 }
 
 function loadAsset(p) {
@@ -116,6 +65,29 @@ function loadAsset(p) {
     });
 }
 
+function perspectiveWarp(raster) {
+    //const keep = new Group();
+    //keep.addChild(raster);
+    //rasterLetter.scale(1);
+    //rasterLetter.fullySelected = true;
+    const letterParts = new Group();
+    for (let i = 0; i < raster.bounds.height; i++) {
+        const part = raster.getSubRaster(new Rectangle([0, i], [raster.bounds.width * 2, i + 1]));
+        letterParts.addChild(part);
+    }
+
+    letterParts.children.reverse();
+    letterParts.children.forEach((l, i) => {
+        // og 2
+        const scale =  .95 / 40 * (i - 0) + 1;
+        l.scale(scale, 1, l.bounds.center);
+    });
+    const partsFinal = letterParts.rasterize();
+    letterParts.remove();
+    raster.remove();
+    return partsFinal;
+}
+
 const sketch = () => {
     return ({ context, width, height }) => {
         context.fillStyle = 'white';
@@ -124,37 +96,56 @@ const sketch = () => {
 
         Promise.all([loadAsset('key.svg'), loadAsset('letters.svg')])
             .then(([key, letters]) => {
-                const g = new Group();
-                //const l = letters.children[65].copyTo(g);
-                const letters1 = utils.shuffle(letters.children);
-                const tiltedLetters = letters1.map(l => {
-                    if (l._type == 'rectangle') return null;
-                    if (l.className == 'CompoundPath') {
-                        const subPaths = l.children.map(path => tilt(path));
-                        return new CompoundPath({
-                            children: [...subPaths],
-                            fillColor: 'black'
-                        });
+                const ogLayer = project.layers[0];
+
+                console.log({letters});
+                let j = 0;
+                letters.children.forEach((l, i) => {
+                    if (l.bounds.width + l.bounds.height > 100) {
+                        l.remove();
                     }
-                    console.log({l});
-                    l.copyTo(g);
-                    return tilt(l);
                 });
-                //l.scale(4);
-                //l.fullySelected = true;
+
+                const rasterLetters = letters.children.map(l => {
+                    const raster = l.rasterize();
+                    //raster.fullySelected = true;
+                    ogLayer.addChild(raster);
+                    return raster;
+                });
                 letters.remove();
-                key.remove();
-		//l.segments[1].transform(new Matrix(1, 1, 1, 1, 1, 1));
-		//l.segments
-		    //l.transform(new Matrix(1, 1, 0, 0, -10, 0));
-		//l.segments.forEach(s => {
-		//});
 
+                const indexsToRemove = [];
+                const comboLetters = [];
+                for(let i = 1; i < rasterLetters.length; i++) {
+                    const length = rasterLetters[i].bounds.center.subtract(rasterLetters[i - 1].bounds.center).length;
+                    if (length < 40) {
+                        const combo = new Group();
+                        combo.addChild(rasterLetters[i]);
+                        combo.addChild(rasterLetters[i - 1]);
+                        const r = combo.rasterize();
+                        combo.remove();
+                        comboLetters.push(r);
+                        indexsToRemove.push(i);
+                        indexsToRemove.push(i - 1);
+                    }
+                    //rasterLetters[i].fullySelected = true;
+                    //console.log({length, a: rasterLetters[i].bounds.center, b: rasterLetters[i - 1].bounds.center});
+                }
 
-                //lMatrix(a, b, c, d, tx, ty)
-                // drawGrid(key);
+                const xxx = [...rasterLetters.filter((l, i) => !indexsToRemove.includes(i)), ...comboLetters];
+                console.log({xxx});
+                project.clear();
+                const warps = xxx.map((r) => {
+                    return perspectiveWarp(r);
+                });
+                project.clear();
+                //letters.remove();
+
+                utils.shuffle(warps);
+                drawGrid(key, warps);
+                //warps.forEach(x => ogLayer.addChild(x));
+                //warps.forEach(x => x.bringToFront());
             });
-
 
         utils.paperJsDraw();
     };
